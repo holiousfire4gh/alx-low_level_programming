191bf5956b0de7fe52e06bdb47c6f62e32b567e4
holiousfire4gh <alamptey689@gmail.com>
2023-08-13 15:26:14 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
- char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ";
+ char alp[52i] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int i;
 
          for (i = 0; i < 52; i++)
          {
-               putchar(alp[i]);
+		 putchar(alp[i]);
          }
           putchar('\n');
           return (0);
