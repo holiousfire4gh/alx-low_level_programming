21fd2f227cc959d867970a99625731406c856bac
holiousfire4gh <alamptey689@gmail.com>
2023-08-18 22:25:40 -0700

    10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - entry point
+ * Description: prints a triangle
+ * @size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
