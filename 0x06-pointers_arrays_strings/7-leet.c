4d9ea502e2b0b84d3d84fbdc40c259c723028aa0
holiousfire4gh <alamptey689@gmail.com>
2023-08-24 05:42:22 -0700

    100-rot13

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,36 @@
 #include "main.h"
-/**
-*leet - encode into 1337speak
-*@n: input value
-*Return: n value
-*/
-char *leet(char *n)
+#include <stdio.h>
+
+char *leet(char *s);
+
+int main(void)
+{
+	char s[] = "hello world";
+	char *p;
+
+	p = leet(s);
+	printf("%s\n", p);
+	return (0);
+}
+
+char *leet(char *s)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	char *p;
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-			if (n[i] == s1[j])
+			if (s[i] == letters[j])
 			{
-				n[i] = s2[j];
+				s[i] = numbers[j];
+				break;
 			}
 		}
 	}
-	return (n);
+	p = s;
+	return (p);
 }
