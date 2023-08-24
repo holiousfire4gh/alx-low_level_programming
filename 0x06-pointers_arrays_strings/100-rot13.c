5a3eebae33d380ac49df743e44f911107ea523b8
holiousfire4gh <alamptey689@gmail.com>
2023-08-24 06:03:22 -0700

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,28 @@
 #include <stdio.h>
 
 /**
-*rot13 - encoder rot13
-*@s: pointer to string params
-*Return: *s
-*/
-
+ * rot13 - encodes a string using ROT13 cipher
+ * @s: pointer to string to be encoded
+ *
+ * Return: pointer to encoded string
+ */
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char datal[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz";
-	char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLmnoprstuvwxyzabcdefghijklm";
+	int i, j;
+	char *p = s;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; alpha[j] != '\0'; j++)
 		{
-			if (s[i] == datal[j])
+			if (s[i] == alpha[j])
 			{
-				s[i] = datarot[j];
+				s[i] = rot13[j];
 				break;
 			}
 		}
 	}
-	return (s);
+	return (p);
 }
